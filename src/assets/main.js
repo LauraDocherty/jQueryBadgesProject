95566bc89b78be6519dca6afcd87170d42c02855
Laura Docherty <bunnylld@hotmail.com>
2017-02-15 10:28:58 +0000

    added titles

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,18 @@
 $(function() {
 
   // your code will go here
-
+    $.ajax({
+    url: 'https://www.codeschool.com/users/sheepyantics.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+	  var element = document.getElementById('badges');
+	  var innerHtml = "";
+	  for(i=0; i<response.courses.completed.length; i++)
+	  {
+		  innerHtml += '<div class="course"> <h3>'+ response.courses.completed[i].title + '</h3><img>'+ +'</div>';
+	  }
+	  element.innerHTML = innerHtml;
+    }
+  });
 });
